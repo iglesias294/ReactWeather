c9e16dc8c191df2ec90deea7b3b534314412fa25
Arturo <iglesias294@icloud.com>
2017-11-19 02:23:57 +0800

    changed api to https

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -1,6 +1,6 @@
 var axios = require('axios');
 
-const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=a882e235b7822251893bcb12605f9158&units=imperial';
+const OPEN_WEATHER_MAP_URL = 'https://api.openweathermap.org/data/2.5/weather?appid=a882e235b7822251893bcb12605f9158&units=imperial';
 
 
 module.exports = {
