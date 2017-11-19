05269a40929de3512080d567c09f0f267919db04
Arturo <iglesias294@icloud.com>
2017-11-19 11:27:43 +0800

    added Foundation and began styling

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -2,15 +2,52 @@ var React = require('react');
 var {Link, IndexLink} = require('react-router');
 
 
-var Nav = (props) => {
-  return (
-    <div>
-      <h2>The navbar</h2>
-      <IndexLink to="/" activeClassName="active" style={{margin: 10}}> Get Weather </IndexLink>
-      <Link to="/about" activeClassName="active" style={{margin: 10}}> About </Link>
-      <Link to="/examples" activeClassName="active" style={{margin: 10}}> Examples </Link>
-    </div>
-  );
-};
+var Nav = React.createClass({
+  onSearch: function(e) {
+    e.preventDefault();
+    alert('Not yet wired up');
+  },
+  render: function () {
+    return (
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">React Weather App</li>
+            <li>
+              <IndexLink to="/" activeClassName="active"> Get Weather </IndexLink>
+            </li>
+            <li>
+              <Link to="/about" activeClassName="active"> About </Link>
+            </li>
+            <li>
+              <Link to="/examples" activeClassName="active"> Examples </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <form onSubmit={this.onSearch}>
+            <ul className="menu">
+              <li>
+                <input type="search" placeHolder="Search Weather"/>
+              </li>
+              <li>
+                <input type="submit" className="button" value="Get Weather"/>
+              </li>
+            </ul>
+          </form>
+        </div>
+      </div>
+    );
+  }
+});
 
 module.exports = Nav;
+
+// var old = (
+//   <div>
+//     <h2>The navbar</h2>
+//     <IndexLink to="/" activeClassName="active" style={{margin: 10}}> Get Weather </IndexLink>
+//     <Link to="/about" activeClassName="active" style={{margin: 10}}> About </Link>
+//     <Link to="/examples" activeClassName="active" style={{margin: 10}}> Examples </Link>
+//   </div>
+// );
