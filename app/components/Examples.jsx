7e8bba330d7fdf6af3600f706d2a6bc919dc6bfd
Arturo <iglesias294@icloud.com>
2017-11-19 15:44:27 +0800

    added some styles

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,8 +1,20 @@
 var React = require('React');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
   return (
-    <h3>This is the examples component.</h3>
+    <div>
+    <h1 className="text-center">Examples</h1>
+    <p>Here are a few example locations to try out.</p>
+    <ol>
+      <li>
+        <Link to='/?location=Shanghai'>Shanghai</Link>
+      </li>
+      <li>
+        <Link to='/?location=Amsterdam'>Amsterdam</Link>
+      </li>
+    </ol>
+    </div>
   );
 };
 module.exports = Examples;
